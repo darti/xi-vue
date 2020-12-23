4ed9c12c686804c4867ca8a965891751856a5895
Matthieu Dartiguenave <matthieu.dartiguenave@gmail.com>
2020-12-23 15:27:12 +0000

    Move js

---
+++ src-js/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="greetings">
+    <label for="name">Your name</label>
+    <input id="name" placeholder="Rusty" v-model="name" />
+    <button @click="greet(name)">Greeting from rust</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue"
+import init, { greet } from "../../pkg/xi_wasm"
+
+export default defineComponent({
+  props: {
+    msg: String,
+  },
+  async setup() {
+    await init()
+    const name = ref("")
+
+    return {
+      greet,
+      name,
+    }
+  },
+})
+</script>
+
+<style scoped>
+#greetings {
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+}
+#greetings > * {
+  flex: auto;
+  margin: 5px;
+  min-width: 200px;
+  align-self: center;
+  padding: 0px;
+}
+</style>
